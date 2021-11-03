af06345efb436d57328a6e13573bf49c7232e0f2
Phat <pattrickha@gmail.com>
2021-11-03 16:29:05 +0700

    Added Dtos

---
+++ SolarSystem.Data/DTOs/UserDTO.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarSystem.Data.DTOs
+{
+    public class UserDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+
+    public class SignInDTO
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [StringLength(maximumLength: 100, MinimumLength = 2)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(maximumLength: 20, MinimumLength = 6)]
+        public string Password { get; set; }
+    }
+
+    public class RegisterDTO : SignInDTO
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required, Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        public IEnumerable<string> Roles { get; set; }
+    }
+}
