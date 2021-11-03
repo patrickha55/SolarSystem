00152dfc306c1c420774957153d1db2e0d5c30d6
Phat <pattrickha@gmail.com>
2021-11-03 18:26:48 +0700

    Implemented Repository

---
+++ SolarSystem.Repository/IRepository/IGenericRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarSystem.Repository.IRepository
+{
+    public interface IGenericRepository<TEntity> where TEntity : class 
+    {
+        Task<IList<TEntity>> GetAllAsync(
+            Expression<Func<TEntity, bool>> expression = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            List<string> includes = null
+            );
+
+        Task<TEntity> GetAsync(
+            Expression<Func<TEntity, bool>> expression,
+            List<string> includes = null
+            );
+
+        Task CreateAsync(TEntity entity);
+        void Update(TEntity entity);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ SolarSystem.Repository/Repository/UnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using SolarSystem.Data;
+using SolarSystem.Data.Entities;
+using SolarSystem.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarSystem.Repository.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationContext context;
+        private IGenericRepository<Region> regions;
+        private IGenericRepository<Component> components;
+        private IGenericRepository<Body> bodies;
+
+        public UnitOfWork(ApplicationContext context)
+        {
+            this.context = context;
+        }
+
+        public IGenericRepository<Region> Regions => regions ??= new GenericRepository<Region>(context);
+
+        public IGenericRepository<Component> Components => components ??= new GenericRepository<Component>(context);
+
+        public IGenericRepository<Body> Bodies => bodies ??= new GenericRepository<Body>(context);
+
+        public void Dispose()
+        {
+            this.context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task SaveAsync()
+        {
+            await context.SaveChangesAsync();
+        }
+    }
+}
