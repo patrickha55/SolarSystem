9835f6abdf547055f3c09d22a4a7a22d58512962
Phat <pattrickha@gmail.com>
2021-11-04 10:56:15 +0700

    Added automapper initialier and bodyDTO

--- SolarSystem.Data/DTOs/BodyDTO.cs
+++ SolarSystem.Data/DTOs/BodyDTO.cs
@@ -1,6 +1,34 @@
-﻿namespace SolarSystem.Data.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SolarSystem.Data.DTOs
 {
-    public class BodyDTO
+    public class CommonField
     {
+        [Required]
+        [StringLength(255, MinimumLength = 1)]
+        public string Name { get; set; }
+        [Required]
+        [Range(0.01, double.MaxValue)]
+        public double EarthMass { get; set; }
+        [Required]
+        [Range(0, double.MaxValue)]
+        public double DistanceToTheSun { get; set; }
+    }
+
+    public class BodyDTO : CommonField
+    {
+        public int Id { get; set; }
+    }
+
+    public class BodyDetailDTO : BodyDTO
+    {
+        public RegionDTO Region { get; set; }
+        public ComponentDTO Component { get; set; }
+    }
+
+    public class ManageBodyDTO : CommonField
+    {
+        public int ComponentId { get; set; }
+        public int RegionId { get; set; }
     }
 }
